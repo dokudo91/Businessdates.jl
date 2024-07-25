45f11eed7633e7e3854a82db7181d75ede0658c3
dokudo91 <92647519+dokudo91@users.noreply.github.com>
2024-07-25 10:35:28 +0900

    ドキュメントを作成

--- src/dates.jl
+++ src/dates.jl
@@ -5,18 +5,8 @@ include("artifacts.jl")
     load_holidaydf()
 
 ```jldoctest
-julia> load_holidaydf()
-1032×2 DataFrame
-  Row │ 国民の祝日・休日月日  国民の祝日・休日名称 
-      │ Date                  String               
-──────┼────────────────────────────────────────────
-    1 │ 1955-01-01            元日
-    2 │ 1955-01-15            成人の日
-    3 │ 1955-03-21            春分の日
-  ⋮   │          ⋮                     ⋮
- 1031 │ 2025-11-23            勤労感謝の日
- 1032 │ 2025-11-24            休日
-                                  1027 rows omitted
+julia> load_holidaydf()[1, 1]
+1955-01-01
 ```
 """
 function load_holidaydf()
