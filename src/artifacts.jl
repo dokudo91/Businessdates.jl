4c27b071ea9f5109404893b662afbadfd4ae4317
dokudo91 <92647519+dokudo91@users.noreply.github.com>
2024-07-25 13:47:41 +0900

    Artifacts.tomlにdownloadを追加

--- src/artifacts.jl
+++ src/artifacts.jl
@@ -1,13 +1,17 @@
 using Pkg.Artifacts
+using SHA
+
+const csvurl = "https://www8.cao.go.jp/chosei/shukujitsu/syukujitsu.csv"
+const csvfilename = splitdir(csvurl)[2]
 
 function bind_artifacts()
     artifact_toml = joinpath(@__DIR__, "Artifacts.toml")
+    csvfilepath = download(csvurl)
+    sha256hex = bytes2hex(open(sha256, csvfilepath))
     hash = create_artifact() do artifact_dir
-        url = "https://www8.cao.go.jp/chosei/shukujitsu/syukujitsu.csv"
-        download(url, joinpath(artifact_dir, "syukujitsu.csv"))
+        mv(csvfilepath, joinpath(artifact_dir, csvfilename))
     end
-    bind_artifact!(artifact_toml, "syukujitsu", hash)
-    hash
+    bind_artifact!(artifact_toml, "syukujitsu", hash; download_info=[(csvurl, sha256hex)])
 end
 
 syukujitsucsv_path() = joinpath(artifact"syukujitsu", "syukujitsu.csv")
